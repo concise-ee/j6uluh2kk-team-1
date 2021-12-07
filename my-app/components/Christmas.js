de454c17ec8d46d636efa312f3d6004ebf29a8a0
Nusaeb Nur Alam <nusaeb.alam@easypark.net>
2021-12-07 14:58:34 +0200

    commit yep yep

--- my-app/components/Christmas.js
+++ my-app/components/Christmas.js
@@ -3,7 +3,6 @@ import React from 'react'
 export const Christmas = ({christmas}) => {
     return(
         <div className="container">
-            <h2>Is it hell yet? {christmas.isItChristmasYet ? 'Yep' : 'Nope'}</h2>
-        </div>
+            <h2>Is it hell yet? {christmas.isItChristmasYet ? 'Yep yep' : 'Nope'}</h2></div>
     )
 }
